aa23bd921374d2ad8ce9a1878ba1529dee626340
Paúl Cevallos <pcevallos@digitalstrategy.com.ec>
2017-08-08 10:58:39 -0500

    prueba

--- bd.log.servicios/Interfaces/ILogEntryService.cs
+++ bd.log.servicios/Interfaces/ILogEntryService.cs
@@ -7,6 +7,7 @@ namespace bd.log.servicios.Interfaces
 {
     public interface ILogEntryService
     {
+
         LogEntry GetLogEntry(int logEntryId,string userName, string applicationName);
         List<LogEntry> GetLogEntries(string userName, string applicationName);
         List<LogEntry> GetLogEntries(DateTime startDate, DateTime FechaFin, string userName, string applicationName);
